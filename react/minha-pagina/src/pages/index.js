0317c719416202f35d88a816bfbd71eed034b03d
BrunoDark777 <106707123+BrunoDark777@users.noreply.github.com>
2025-09-22 00:00:11 -0300

    Hello world com react.js

--- react/minha-pagina/src/pages/index.js
+++ react/minha-pagina/src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import estilos from "@/styles/Home.module.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,8 +22,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <h1>Hello World</h1>
+      <div className={estilos.container_principal}>
+        <div className={estilos.container_texto}>
+          <h1>Hello World</h1>
+          <p>Parágrafo com React.js</p>
+          <p className={estilos.autor}>Criado por Bruno César</p>
+        </div>
       </div>
     </>
   );
