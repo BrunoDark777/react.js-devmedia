815692d2a336c00207a39afc509b9798f9bc2265
BrunoDark777 <106707123+BrunoDark777@users.noreply.github.com>
2025-10-14 00:41:42 -0300

    Componentes React

---
+++ projeto-1/minha-pagina/src/componentes/Topo/index.jsx
@@ -0,0 +1,9 @@
+import estilos from './Topo.module.css'
+
+export default function Topo(){
+    return(
+        <header className={estilos.container_topo}>
+            <h1>Trabalhando com Componentes</h1>
+        </header>
+    )
+}
